bf79b2c782ba80a6b7e2f678adfbea66a1d80244
antinoKartik <kartik.c@antino.io>
2024-01-16 01:02:38 +0530

    custom select complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,88 @@
+import { useEffect, useRef } from "react";
+import { useState } from "react";
+import Chip from "./Components/Chip";
+import { allItems } from "./Utilities/LIst";
 
 function App() {
-  return (
-    <div className="App">
+  const [inputValue, setInputValue] = useState("");
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const inputRef = useRef(null);
+  const chipsWrapper = useRef(null);
 
+  useEffect(() => {
+    if (chipsWrapper.current) {
+      chipsWrapper.current.scrollLeft = chipsWrapper.current.scrollWidth;
+    }
+  }, [selectedItems]);
+
+  const updateFilteredItems = () => {
+    const filtered = allItems.filter(
+      (item) =>
+        item.tech.toLowerCase().includes(inputValue.toLowerCase()) &&
+        !selectedItems.some((selectedItem) => selectedItem.id === item.id)
+    );
+    setFilteredItems(filtered);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+    updateFilteredItems();
+  };
+
+  const handleItemClick = (item) => {
+    setSelectedItems([...selectedItems, item]);
+    setInputValue("");
+    updateFilteredItems();
+  };
+
+  const handleChipRemove = (itemToRemove) => {
+    const updatedItems = selectedItems.filter((item) => item !== itemToRemove);
+    setSelectedItems(updatedItems);
+    updateFilteredItems();
+  };
+
+  const handleBackSpace = (e) => {
+    if (e.key === "Backspace") {
+      // Handle backspace key press
+      if (selectedItems.length > 0 && inputValue) {
+        console.log(inputValue, "Backspace key pressed");
+      }
+    }
+  };
+
+  return (
+    <div className="w-full pt-20 px-44">
+      <div
+        className="flex gap-2 items-center border-b border-black px-2 overflow-x-scroll no-scrollbar  w-[50%] "
+        ref={chipsWrapper}
+      >
+        {selectedItems.map((item, index) => (
+          <Chip
+            handleChipRemove={handleChipRemove}
+            item={item}
+            index={index}
+            isSelected={true}
+          />
+        ))}
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Type here.."
+          ref={inputRef}
+          className="border-none outline-none h-[3.5rem] mr-9"
+          onKeyDown={handleBackSpace}
+        />
+      </div>
+
+      {filteredItems.length > 0 && inputValue && (
+        <div className="max-h-[16rem] overflow-y-scroll mt-2 shadow-lg bg-slate-50 w-[50%]  no-scrollbar ml-2">
+          {filteredItems.map((item, index) => (
+            <Chip handleItemClick={handleItemClick} item={item} index={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Components/Chip.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Chip = ({
+  item,
+  index,
+  handleItemClick,
+  isSelected,
+  handleChipRemove,
+}) => {
+  return (
+    <div
+      key={index}
+      onClick={() => {
+        if (!isSelected) handleItemClick(item);
+      }}
+      className={`${
+        isSelected ? " rounded-full bg-slate-200" : "cursor-pointer"
+      } flex gap-2 items-center p-2 `}
+    >
+      <div className="rounded-full bg-green-700 w-[2rem] h-[2rem] flex justify-center items-center">
+        {" "}
+        {item.icon}
+      </div>
+      <p className="whitespace-nowrap">{item?.tech}</p>
+      {isSelected && (
+        <span
+          onClick={() => handleChipRemove(item)}
+          className="px-2 cursor-pointer"
+        >
+          X
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Chip;
